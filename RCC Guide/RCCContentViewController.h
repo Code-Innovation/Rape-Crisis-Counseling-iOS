e8c8380880d85174d6ff5f8ad76b81ec09e3e538
Alexandr Afonin <alexandr.afonin@sibers.com>
2018-02-07 19:35:52 +0600

    ui

---
+++ RCC Guide/RCCContentViewController.h
@@ -0,0 +1,15 @@
+//
+//  RCCContentViewController.h
+//  RCC Guide
+//
+//  Created by Alexandr Afonin on 2/7/18.
+//  Copyright © 2018 Afonin Alexandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <LGSideMenuController/LGSideMenuController.h>
+
+@interface RCCContentViewController : LGSideMenuController
+
+@end
